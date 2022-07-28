5e4cd830c11a6ece83bd63c7e691ad0cdb893867
Zzl3 <91377542+Zzl3@users.noreply.github.com>
2022-07-28 21:57:01 +0800

    Update index.js

--- Code_2/MeeTeam-Font/src/axios/index.js
+++ Code_2/MeeTeam-Font/src/axios/index.js
@@ -10,7 +10,7 @@ axios.defaults.withCredentials = true
 // create an axios instance
 const service = axios.create({
     //baseURL: process.env.VUE_APP_BASE_API, // url = base url + request url
-    baseURL:'https://localhost:44390/',
+    baseURL:'https://localhost:44338/',
     // baseURL:'http://192.168.43.153:6001/api/',这个应该是把后端部署了
     // withCredentials: true, // send cookies when cross-domain requests
     timeout: 50000, // request timeout
