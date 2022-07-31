b3d8fd81067e85b2a8679fc4f9c8d88feefa364c
Zzl3 <91377542+Zzl3@users.noreply.github.com>
2022-07-31 16:37:07 +0800

    组队需求后端

--- Code_2/Meeteam_Backend/Meeteam_Backend/Controllers/LoginController.cs
+++ Code_2/Meeteam_Backend/Meeteam_Backend/Controllers/LoginController.cs
@@ -22,16 +22,10 @@ namespace MeeTeam_Back.Controllers
         {
             UserInfo info = new UserInfo();
             string real_psw = info.get_Password(user_id);
-            /* if (real_psw == null || real_psw != password)
-                 return false;
-             else 
-                 return true;*/
-            if (user_id == "admin" && password == "admin")
-            {
-                return true;
-            }
-            else
+            if (real_psw == null || real_psw != password)
                 return false;
+            else
+                return true;
         }
     }
 }
