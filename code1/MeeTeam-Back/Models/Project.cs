b038f9bbde08e09e2e82802fe18854d38ba2a198
Luoyao176 <85424272+Luoyao176@users.noreply.github.com>
2022-08-06 23:48:25 +0800

    Add files via upload

---
+++ code1/MeeTeam-Back/Models/Project.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Data;
+using Oracle.ManagedDataAccess.Client;
+using System.Data.SqlClient;
+using SqlSugar;
+
+
+namespace Meeteam_Backend.Models
+{
+    [SugarTable("project")]
+    public class Project
+    {
+        [SugarColumn(IsPrimaryKey = true)]
+        public string project_id { get; set; }
+
+        [SugarColumn(IsJson = true)]
+        public string project_name { get; set; }
+        public string project_background { get; set; }
+        public string project_introduction { get; set; }
+        public string project_content { get; set; }
+        public string project_status { get; set; }
+        public string create_time { get; set; }
+        public string due { get; set; }
+        public string project_progress { get; set; }
+        public string admin_id { get; set; }
+        public char audit_result { get; set; }
+        public string audit_time { get; set; }
+
+    }
+}
---
+++ code1/MeeTeam-Back/Models/User.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Oracle.ManagedDataAccess.Client;
+using SqlSugar;
+
+namespace MeeTeam_Backend
+{   
+    [SugarTable("user_info")]
+    public class UserInfo
+    {
+           [SugarColumn(IsPrimaryKey = true)]
+           public string user_id { get; set; }
+
+           [SugarColumn(IsJson = true)]
+           public string password { get; set; }
+           public string register_time { get; set; }
+           public char account_status { get; set; }
+           public string user_name { get; set; }
+           public char gender { get; set; }
+           public string contact_info { get; set; }
+           public string institution { get; set; }
+           public string major { get; set; }
+           public string grade { get; set; }
+           public string introduction { get; set; }
+           public int point { get; set; } //表中是number(38,0),int位数不足，暂时先不考虑大数
+
+        public string get_Password(string user_id)
+        {
+            string password = null;
+            dbORM dborm = new dbORM();
+            SqlSugarClient db = dborm.getInstance();//获取数据库连接
+            //查询
+            var list = db.Queryable<User_Info>()
+                .Select(it => it.password)
+                .Where("user_id=@id", new { id = user_id })
+                .ToList();
+            if (list.Count>0&&list[0] != null  )
+            {
+                password = list[0].ToString();
+                return password;
+            }
+            else
+                return "0";
+        }
+    }
+}
