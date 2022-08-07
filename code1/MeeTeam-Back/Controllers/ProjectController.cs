85ed265baab63e01ba311f006e1303ae7ebba3dd
reverse-47 <85037574+reverse-47@users.noreply.github.com>
2022-08-07 23:58:27 +0800

    更新了项目选择

--- code1/MeeTeam-Back/Controllers/ProjectController.cs
+++ code1/MeeTeam-Back/Controllers/ProjectController.cs
@@ -10,60 +10,90 @@ using System.Data;
 using System.Collections;
 using SqlSugar;
 using MeeTeam_Backend;
-using Meeteam_Backend.Models;
 
 namespace Meeteam_Backend.Controllers
 {
     [Route("/[Controller]/[action]")]
     [ApiController]
     [EnableCors("any")]
+
     public class ProjectController : ControllerBase
     {
-        //上传项目
+        //新建项目
         [HttpPost]
-        public bool AddProject(string project_id, string project_name, string project_background, string project_introduction, string project_content, string project_status,string due,string project_progress)
+        public bool AddProject(string project_id, string project_name, string project_background, string project_info, string project_content, string project_status, string project_stime, string project_etime, string project_progress)
         {
-            //获取数据库连接
             dbORM dborm = new dbORM();
-            SqlSugarClient db = dborm.getInstance();
+            SqlSugarClient db = dborm.getInstance();//获取数据库连接
+                                                    //添加
 
-            Project pos = new Project();
-            pos.project_id = project_id;
-            pos.project_name = project_name;
-            pos.project_background = project_background;
-            pos.project_introduction = project_introduction;
-            pos.project_content = project_content;
-            pos.project_status = project_status;
-            pos.create_time = DateTime.Now.ToString("g"); //2009/10/30 20:40;
-            pos.due = due;
-            pos.project_progress = project_progress;
-
-            int count = db.Insertable(pos).ExecuteCommand();
-            if (count == 1)
-                return true;
-            else
+            try
+            {
+                Project pos = new Project();
+                pos.project_id = project_id;
+                pos.project_name = project_name;
+                pos.project_background = project_background;
+                pos.project_info = project_info;
+                pos.project_content = project_content;
+                pos.project_status = project_status;
+                pos.project_stime = project_stime;
+                pos.project_etime = project_etime;
+                pos.project_progress = project_progress;
+                if (db.Insertable(pos).ExecuteCommand() > 0)
+                    return true;
+                else
+                    return false;
+            }
+            catch (Exception ex)
+            {
                 return false;
+            }
+        }
+        //获取全部项目
+        [HttpGet]
+        public List<Project> SelectAllProject()
+        {
+            dbORM dborm = new dbORM();
+            SqlSugarClient db = dborm.getInstance();//获取数据库连接
+            try
+            {
+                List<Project> plist = db.Queryable<Project>().ToList();
+                return plist;
+            }
+            catch (Exception ex)
+            {
+                return null;
+            }
+        }
+        [HttpGet]
+        //获取单条项目信息
+        public Project get_by_project_id(string project_id)
+        {
+            Project project = new Project();
+            dbORM dborm = new dbORM();
+            SqlSugarClient db = dborm.getInstance();//获取数据库连接
+            project = db.Queryable<Project>().Where(it => it.project_id == project_id).First();
+            return project;
         }
         //删除项目
         [HttpDelete]
         public bool deleteProject(string project_id)
         {
-            //获取数据库连接
             dbORM dborm = new dbORM();
-            SqlSugarClient db = dborm.getInstance();
-            //删除
+            SqlSugarClient db = dborm.getInstance();//获取数据库连接                                      //删除
             try
             {
-                int count = db.Deleteable<Project>().In(project_id).ExecuteCommand();
+                int count = db.Deleteable<Grouping_Requirement>().In(project_id).ExecuteCommand();
                 if (count == 1)
                     return true;
                 else
                     return false;
             }
-            catch (Exception)
+            catch (Exception ex)
             {
                 return false;
             }
         }
     }
 }
+
