0858dc78662a16017e679f8e7177d9dcfa863937
reverse-47 <85037574+reverse-47@users.noreply.github.com>
2022-08-12 22:16:17 +0800

    Add files via upload

--- code1/MeeTeam-Font/src/api/Inforlist.js
+++ code1/MeeTeam-Font/src/api/Inforlist.js
@@ -3,7 +3,7 @@ import request from '@/utils/request'
   export function getlistInfor() {
     /*组队需求的表格*/
     return request({
-      url: '/GroupRequirement/SelectAllGroupRequirement',
+      url: '/Project/SelectAllProject',
       method: 'get',
     })
   }
