516375ba62a2a53b27ff70078fbe833019cd287c
reverse-47 <85037574+reverse-47@users.noreply.github.com>
2022-08-19 19:55:46 +0800

    Add files via upload

--- code1/MeeTeam-Font/src/router/index.js
+++ code1/MeeTeam-Font/src/router/index.js
@@ -5,21 +5,44 @@ import Router from 'vue-router'
 Vue.use(Router)
 
 //导入需要的组件
-import LogIn from '@/components/Login.vue'
-import LogOut from '@/components/Logout.vue'
-import InforList from '@/components/InforList.vue'
-import PostInfor from '@/components/PostInfor.vue'
-import CreatList from '@/components/CreatList.vue'
-import PersonInfo from '@/components/PersonInfo.vue'
+import LogIn from '@/view/Login.vue'
+import InforList from '@/view/InforList.vue'
+import CreatList from '@/view/CreatList.vue'
+import MyProject from '@/view/MyProject.vue'
+import AddUser from '@/view/AddUser.vue'
+import AdminPage from '@/view/AdminPage.vue'
+import NoticeList from '@/view/NoticeList.vue'
+import ProjectDetail from '@/view/ProjectDetail.vue'
+import ChangeList from '@/view/ChangeList.vue'
+import ShowNotice from '@/view/ShowNotice.vue'
+import CreateNotice from '@/view/CreateNotice.vue'
 export default new Router({
     //在这里声明路由规则
     routes: [
-        {path: '/', redirect: '/users'},
-        {path: '/users', component: LogIn},
-        {path: '/users/LogOut', component: LogOut},
-        {path: '/users/InforList', component: InforList},
-        {path: '/users/PostInfor', component: PostInfor},
-        {path: '/users/CreatList', component: CreatList},
-        {path: '/users/PersonInfo',component: PersonInfo},
-    ],
-})
+        { path: '/', redirect: '/login' },
+        { path: '/login', component: LogIn },
+        {
+            path: '/users/InforList', name: 'InforList', component: InforList, props: true, meta: {
+                keepAlive: true
+            }
+        },
+        {
+            path: '/users/ProjectDetail', name: 'ProjectDetail', component: ProjectDetail, props: true, meta: {
+                keepAlive: true
+            }
+        },
+        { path: '/users/CreatList', component: CreatList },
+        {
+            path: '/users/MyProject', name: 'MyProject', component: MyProject, props: true, meta: {
+                keepAlive: true
+            }
+        },
+        { path: '/users/ChangeList', name: 'ChangeList', component: ChangeList, props: true },
+        { path: '/users/AddUser', component: AddUser },
+        { path: '/Admin/AdminPage', component: AdminPage, props: true },
+        { path: '/Admin/NoticeList', component: NoticeList, props: true },
+        { path: '/Admin/ShowNotice', component: ShowNotice, props: true },
+        { path: '/Admin/CreateNotice', component: CreateNotice, props: true }
+    ]
+}
+)
