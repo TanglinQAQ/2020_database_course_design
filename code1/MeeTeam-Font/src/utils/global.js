6382514b050d5aea4ba43b2418647a28d0ac8b14
sbw2053512 <83405314+sbw2053512@users.noreply.github.com>
2022-08-20 17:34:07 +0800

    Update global.js

--- code1/MeeTeam-Font/src/utils/global.js
+++ code1/MeeTeam-Font/src/utils/global.js
@@ -6,4 +6,6 @@ export default{
     evaluationnum:0,//评论个数
     nowadminid:"system",//现在的管理员
     nowuserid:"normal",//现在的userid，也就是用户的名称，设为normal后面好调试
+    ID:"admin",//每次打开个人信息界面时修改ID
+    otherID:"sbw"//访问其他人页面时别人的ID
 }
