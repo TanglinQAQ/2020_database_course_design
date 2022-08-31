4c6d72081fb14156bf2cd6e5b3d0f74b6ce4f530
Crayon-th <snail.cray@gmail.com>
2022-08-31 08:52:51 +0800

    项目图片可插入

--- Code/MeeTeam-Font/src/view/Project_and_Require/ProjList.vue
+++ Code/MeeTeam-Font/src/view/Project_and_Require/ProjList.vue
@@ -10,20 +10,19 @@
 
         <template>
             <div v-if="reset">
-                <el-row :glutter="10" class="elrow">
+                <el-row :glutter="10">
                     <el-col :span="6" v-for="(o, index) in tabledata" :key="index" :offset="2">
                         <div style="margin-top:15px">
                             <!--这一层div的作用是什么-->
                             <el-card :body-style="{ padding: '0px' }" shadow="hover">
-                                <img src="https://shadow.elemecdn.com/app/element/hamburger.9cf7b091-55e9-11e9-a976-7f4d0b07eef6.png"
-                                    class="image" />
+                                <img :src=o.project_img class="image" />
                                 <div class="text">
                                     {{  o.project_name  }}<br />
                                     发起时间：{{  o.create_time  }}<br />
                                     项目进度：{{  o.project_progress  }}<br /><br>
                                     <template>
                                         <el-button type="success" plain size="mini" style="float:right"
-                                        @click="handleLook(index, o.row)">了解更多</el-button>
+                                            @click="handleLook(index, o.row)">了解更多</el-button>
                                     </template>
                                     <br>
                                 </div>
@@ -91,6 +90,7 @@ export default {
                         create_time: '',
                         project_status: '',
                         project_id: '',
+                        project_img: ''
                     }
 
                     form.project_name = item.project_name;
@@ -100,6 +100,17 @@ export default {
                     form.create_time = form.create_time.replace("\"", "").replace("\"", "");//去掉时间格式的引号
                     form.project_status = item.project_status;
                     form.project_id = item.project_id;
+
+                    let param = {
+                        target: "project",
+                        id: form.project_id
+                    }
+
+                    openfile(param).then((res) => {
+                        if (res.data) {
+                            form.project_img = 'data:;base64,' + res.data;
+                        }
+                    })
 
                     vm.tabledata.push(form);
                     // console.log(form);
@@ -121,7 +132,6 @@ export default {
     mounted() {
         this.gettable();
     },
-
 
 }
 </script>
@@ -147,6 +157,24 @@ export default {
     padding: 16px;
     line-height: 30px;
 }
+
+/*
+这里的后两个是对于card自适应高度的一种可能可行的解决方案
+*/
+.el-row {
+    margin-bottom: 20px;
+    display: flex;
+    flex-wrap: wrap;
+}
+
+.el-row .el-card {
+    min-width: 100%;
+    height: 100%;
+    margin-right: 20px;
+    transition: all .5s;
+}
+
+
 </style>
 
 <style scoped>
@@ -158,9 +186,6 @@ export default {
 .bottom {
     margin-top: 13px;
     line-height: 12px;
-}
-
-.button {
     padding: 0;
     float: right;
 }
