aa6c1df3a1b83a19f1b3fa97e9d18352bd3d4d41
TanglinQAQ <3257138524@qq.com>
2022-09-02 04:40:27 +0800

    部署在服务器上的代码，感觉有很多bug

---
+++ Code_On_Server/MeeTeam-Font/src/view/PersonalSpace/HisTory.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>这里是我的浏览历史</div>
+  
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
---
+++ Code_On_Server/Meeteam_Backend/Meeteam_Backend/Controllers/UserController.cs
@@ -0,0 +1,82 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using MeeTeam_Back;
+using Oracle.ManagedDataAccess.Client;
+using Microsoft.AspNetCore.Cors;
+using SqlSugar;
+using System.Data;
+using System.Collections;
+using MeeTeam_Backend;
+
+namespace Meeteam_Backend.Controllers
+{
+    [Route("/[Controller]/[action]")]
+    [ApiController]
+    [EnableCors("any")]
+    public class UserController : ControllerBase
+    {
+        [HttpPost]
+        //添加浏览历史
+        public bool AddViewHistory(string user_id, string project_id)
+        {
+            dbORM dborm = new dbORM();
+            SqlSugarClient db = dborm.getInstance();//获取数据库连接
+
+            try
+            {
+                Browsehistory bh = new Browsehistory();
+                bh.user_id = user_id;
+                bh.project_id = project_id;
+                bh.browse_time = DateTime.Now.ToString();
+
+                int count = db.Storageable(bh).ExecuteCommand(); //自动判断是insert还是update
+                if (count == 1)
+                {
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
+                /*
+                int count = db.Insertable(bh).ExecuteCommand();
+                if (count == 1)
+                    return true;
+                else
+                {
+                    
+                     * int count2 = db.Updateable<Browsehistory>()
+                        .SetColumns(p=>new Browsehistory() { browse_time = bh.browse_time })
+                        .Where(it => it.user_id == user_id && it.project_id == project_id)
+                        .ExecuteCommand();
+                     
+                    int count2 = db.Updateable<Browsehistory>()
+                        .SetColumns(it => new Browsehistory { browse_time = bh.browse_time })
+                        .Where(it => it.project_id == project_id&&it.user_id==user_id)
+                        .ExecuteCommand();
+
+                    //int count2 = db.Updateable(bh).ExecuteCommand();
+                    if (count2 == 1)
+                    {
+                        return true;
+                    }
+                    else
+                    {
+                        return false;
+                    }
+                
+                }
+                    */
+            }
+            catch (Exception ex)
+            {
+                return false;
+            }
+
+        }
+    }
+}
